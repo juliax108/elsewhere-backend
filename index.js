4bf3c18b29cc809b425b813de38227a6d23c5cad
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-06-10 20:25:45 +0200

    MongoDB Verbindung hinzugefügt

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
-
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/elsewhere')
+    .then(() => console.log('Datenbank verbunden'))
+    .catch((err) => console.log('Datenbankfehler:', err))
 
 app.get('/trips', (req, res) => {
     res.send('alle Trips');
@@ -15,4 +19,6 @@ app.get('/trips', (req, res) => {
 app.listen(port, () => {
     console.log(`Server läuft auf Port ${port}`);
 });
+
+
 
