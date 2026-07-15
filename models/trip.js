ff03620b0eb4a9bf2e919b65dfcce244a2cbd95a
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-07-15 12:30:40 +0200

    Trip-Modell um Status und Stationen erweitert

--- models/trip.js
+++ models/trip.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose');
 
 const tripSchema = new mongoose.Schema({
     title: String,
-    country: String,
-    city: String,
+    status: {
+        type: String,
+        enum: ['geplant', 'abgeschlossen'],
+        default: 'geplant'
+    },
     startDate: Date,
     endDate: Date,
     description: String,
     rating: Number,
     travelWith: String,
     transportMode: String,
+    stops: [
+        {
+            country: String,
+            city: String
+        }
+    ]
 })
 
 module.exports = mongoose.model('Trip', tripSchema);
